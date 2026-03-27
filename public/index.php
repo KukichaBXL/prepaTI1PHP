c305ce9384202ddefaf595f3f4a8fef8a76e2f3f
KukichaBXL <craeyerobin@gmail.com>
2026-03-27 12:07:18 +0100

    First push EX-CF

--- public/index.php
+++ public/index.php
@@ -9,3 +9,18 @@ le srcipt en cas d'échec
 require_once "../config.php";
 
 
+if(!isset($_GET['page'])) {
+    
+    include ROOT_PATH."/view/index.php";
+
+
+}else if(in_array($_GET['page'],PUBLIC_PAGES)){
+
+    
+    include ROOT_PATH."/view/".$_GET['page'].".php";
+}
+
+else {
+   
+    include ROOT_PATH."/view/404.php";
+}
---
+++ view/inc/navbar.php
@@ -0,0 +1,41 @@
+<nav id="navmenu" class="navmenu">
+          <ul>
+            <li><a href="./" class="active">Home</a></li>
+            <li><a href="?page=about">About</a></li>
+            <li><a href="?page=departments">Departments</a></li>
+            <li><a href="?page=services">Services</a></li>
+            <li><a href="?page=doctors">Doctors</a></li>
+            <li class="dropdown"><a href="#"><span>More Pages</span> <i class="bi bi-chevron-down toggle-dropdown"></i></a>
+            <ul>
+              <li><a href="?page=department-details">Department Details</a></li>
+              <li><a href="?page=service-details">Service Details</a></li>
+              <li><a href="?page=appointment">Appointment</a></li>
+              <li><a href="?page=testimonials">Testimonials</a></li>
+              <li><a href="?page=faq">Frequently Asked Questions</a></li>
+              <li><a href="?page=gallery">Gallery</a></li>
+              <li><a href="?page=terms">Terms</a></li>
+              <li><a href="?page=privacy">Privacy</a></li>
+              <li><a href="?page=404">404</a></li>
+            </ul>
+            </li>
+            <li class="dropdown"><a href="#"><span>Dropdown</span> <i class="bi bi-chevron-down toggle-dropdown"></i></a>
+              <ul>
+                <li><a href="#">Dropdown 1</a></li>
+                <li class="dropdown"><a href="#"><span>Deep Dropdown</span> <i class="bi bi-chevron-down toggle-dropdown"></i></a>
+                  <ul>
+                    <li><a href="#">Deep Dropdown 1</a></li>
+                    <li><a href="#">Deep Dropdown 2</a></li>
+                    <li><a href="#">Deep Dropdown 3</a></li>
+                    <li><a href="#">Deep Dropdown 4</a></li>
+                    <li><a href="#">Deep Dropdown 5</a></li>
+                  </ul>
+                </li>
+                <li><a href="#">Dropdown 2</a></li>
+                <li><a href="#">Dropdown 3</a></li>
+                <li><a href="#">Dropdown 4</a></li>
+              </ul>
+            </li>
+            <li><a href="?page=contact">Contact</a></li>
+          </ul>
+          <i class="mobile-nav-toggle d-xl-none bi bi-list"></i>
+        </nav>
